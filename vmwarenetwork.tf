e8820698c5a36f3a13798ed8390bef10c1a1f7f3
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-11 01:29:24 +0530

    Add files via upload

---
+++ vmwarenetwork.tf
@@ -0,0 +1,24 @@
+provider "vsphere" {
+  user           = "cmpqa.svc@itomcmp.servicenow.com"
+  password       = "snc!23$"
+  vsphere_server = "10.198.1.13"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "devcloud"
+}
+
+resource "vsphere_distributed_virtual_switch" "dvs" {
+  name          = "terraform-test-dvs"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_distributed_port_group" "pg" {
+  name                            = "terraform-test-pg"
+  distributed_virtual_switch_uuid = "${vsphere_distributed_virtual_switch.dvs.id}"
+
+  vlan_id = 1000
+}
